d359341004c4c8346c907e438590c563c161704d
Dynameiz <hanz141204@gmail.com>
2025-06-29 23:55:45 +0700

    UI: Added Contact Page

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import Footer from "@/components/ui/Footer";
 import GetStarted from "@/components/ui/GetStarted";
+import { IconBrandWhatsapp } from "@tabler/icons-react";
 import { DM_Sans } from "next/font/google";
 import Image from "next/image";
 
@@ -16,7 +18,66 @@ export default function Contact() {
     <div className={`${dmSans.className} relative flex-col items-center justify-center w-full min-h-screen`}>
       <section className="flex flex-col items-center justify-center w-full px-4 py-8 mx-auto bg-light-beige-2">
         <div className="container flex flex-col items-center justify-center mx-auto mt-24 text-center">
-          <Image src={"/WAG-QR.png"} alt="WAG QR Code" width={200} height={200} className="rounded-lg mb-8" />
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4 p-3 pt-0 border-b-2 border-green">Get in Touch</h1>
+          <p className="text-lg mb-8">
+            Have questions or need assistance? We're here to help! Reach out to us through our WhatsApp community or contact our manager directly.
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-8 max-w-7xl">
+            <div className="flex flex-col justify-around items-center bg-white p-6 rounded-lg shadow-md">
+              <h2 className="text-lg sm:text-xl lg:text-2xl font-bold mb-4 p-1.5 pt-0 border-b-2 border-green">Join Our Community</h2>
+              <p className="mb-4 max-w-md text-center text-gray-500">
+                Connect with us and other members by joining our WhatsApp group community.
+              </p>
+              <Image src={"/WAG-QR.png"} alt="WAG QR Code" width={256} height={256} className="rounded-lg border-border shadow-md mb-4" />
+                <Button
+                asChild
+                variant={"default"}
+                className="w-full max-w-3xs cursor-pointer hover:scale-105 transition-transform duration-200"
+                >
+                <a
+                  href="https://chat.whatsapp.com/Idf2KvLibXhCDXuOBLreyd"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Join Now
+                </a>
+                </Button>
+            </div>
+            <div className="flex flex-col justify-around items-center bg-white p-6 rounded-lg shadow-md">
+              <h2 className="text-lg sm:text-xl lg:text-2xl font-bold mb-4 p-1.5 pt-0 border-b-2 border-green">Contact the Manager</h2>
+              <p className="mb-4 max-w-md text-center text-gray-500">
+                Reach out directly to our manager, Brian, for any inquiries or assistance.
+              </p>
+              <IconBrandWhatsapp size={192} className="text-green" />
+              <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center mb-4">
+                  <span className="font-semibold text-lg">Brian</span>
+                    <Button
+                    asChild
+                    variant="link"
+                    className="p-0 h-auto text-green text-lg"
+                    >
+                    <a
+                      href="tel:08123456789"
+                      className="flex items-center gap-2"
+                    >
+                      <IconBrandWhatsapp size={20} className="inline" />
+                      0812-3456-789
+                    </a>
+                    </Button>
+                </div>
+                <Button
+                  asChild
+                  variant="default"
+                  className="w-full max-w-3xs cursor-pointer hover:scale-105 transition-transform duration-200"
+                >
+                  <a href="tel:08123456789">
+                    Contact Now
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       <GetStarted />
--- app/(public)/plans/page.tsx
+++ app/(public)/plans/page.tsx
@@ -46,7 +46,7 @@ export default function Plans() {
     <div className={`${dmSans.className} relative flex-col items-center justify-center w-full min-h-screen`}>
       <section className="flex flex-col items-center justify-center w-full px-4 mx-auto bg-light-beige-2">
         <div className="container mx-auto flex flex-col items-center justify-center mb-8 mt-16 md:mt-24 py-8">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 p-3 pt-0 border-b-2 border-green">Meal Plans</h1>
+          <h1 className="text-3xl md:text-4xl font-bold mb-4 p-3 pt-0 border-b-2 border-green">Meal Plans</h1>
           <p className="text-lg max-w-2xl md:max-w-full text-center">
             Choose a plan that suits your needs. We offer flexible options to help you get the most out of our services.
           </p>
